6762531af8f8827889cdb3f531479f70e8d64207
Kellen <kellenbmay@gmail.com>
2020-11-16 17:39:03 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    case array.length
+    when 1
+        array.join
+    when 2
+        array.join(" and ")
+    else
+        array_copy = array
+        array_copy[-1] = "and #{array_copy[-1]}"
+        array_copy.join(", ")
+    end
 end
