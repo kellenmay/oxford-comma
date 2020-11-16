ba436132a0122cff2b9311427346b97f34a4bcad
Kellen <kellenbmay@gmail.com>
2020-11-16 17:37:53 -0500

    commit tree 86903531a4eece2e56bc0b88105288b75569b7e9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    case array.length
+    when 1
+        array.join
+    when 2
+        array.join(" and ")
+    else
+        array.join(", ")
+        array.last.join(", and")
+    end
 end
